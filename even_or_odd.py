f5623818948469356ea707dc9c694ff221eac936
Sireesha0907 <48920844+Sireesha0907@users.noreply.github.com>
2019-07-17 10:54:50 +0530

    Update even_or_odd.py

--- even_or_odd.py
+++ even_or_odd.py
@@ -3,10 +3,13 @@ def main():
         a=input()
         print(a)
         n=int(a)
-        if((int(a)%2)==1):
-            print("The Number is Odd")
-        elif((int(a)%2)==0):
+        if(n<0):
+            print("Negative Number")
+        elif((n%2)==0):
             print("The Number is Even")
+        elif((n%2)==1):
+            print("The Number is Odd")
     except ValueError:
         print("Invalid Character")
 main()
+

4c52eab6d9c862e5860d7d341c2099a7faabeead
Sai Sireesha Gopal <48920844+Sireesha0907@users.noreply.github.com>
2019-07-18 15:06:20 +0530

    Update palindrome_integer.py

--- palindrome_integer.py
+++ palindrome_integer.py
@@ -1,11 +1,14 @@
 p=int(input("Enter the Number"))
 tempo=p
 rev=0
-while(p>0):
-    num=p%10
-    rev=rev*10+num
-    p=p//10
-if(tempo==rev):
-    print("The Number is a palindrome")
+if(p<=1000):
+    while(p>0):
+        num=p%10
+        rev=rev*10+num
+        p=p//10
+    if(tempo==rev):
+        print("The Number is a palindrome")
+    else:
+        print("The Number is not a palindrome")
 else:
-    print("The Number is not a palindrome")
+    print("Invalid")
